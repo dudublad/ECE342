0cff446c7c243e738f56afbe6e9aa491f09ef826
Eduardo <edu.ortenblad@gmail.com>
2020-01-20 18:23:03 -0500

    Implemented Lab 1 Part 1

--- lab1_kit/control.sv
+++ lab1_kit/control.sv
@@ -2,16 +2,16 @@ module control
 (
 	input clk,
 	input reset,
-	
+
 	// Button input
 	input i_enter,
-	
+
 	// Datapath
 	output logic o_inc_actual,
 	input i_over,
 	input i_under,
 	input i_equal,
-	
+
 	// LED Control: Setting this to 1 will copy the current
 	// values of over/under/equal to the 3 LEDs. Setting this to 0
 	// will cause the LEDs to hold their current values.
@@ -22,7 +22,12 @@ module control
 // that are of enum type.
 enum int unsigned
 {
-	// TODO: declare states here
+
+	S_FIST,
+	S_SECOND,
+	S_THIRD,
+	S_FOURTH
+
 } state, nextstate;
 
 // Clocked always block for making state registers
@@ -39,9 +44,21 @@ always_comb begin
 	nextstate = state;
 	o_inc_actual = 1'b0;
 	o_update_leds = 1'b0;
-	
+
 	case (state)
 		// TODO: complete this
+		S_FIRST: begin
+			o_inc_actual = 1'b1;
+			if (i_enter) nextstate = S_SECOND;
+		end
+		S_SECOND: begin
+			if (i_enter) nextstate = S_THIRD;
+		end
+		S_THIRD: begin
+			o_update_leds = 1'b0;
+			if (i_equal) nextstate = S_FOURTH;
+			else nextstate = S_SECOND;
+		end
 	endcase
 end
 
